fb9c8080fc347df9e5bdb2e0bccfab886f23ba29
Mauricio Silveira <mauricio.machado.silveira@gmail.com>
2018-01-26 10:56:58 -0500

    First commit with file structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chat App #
+
+This is a chat app for Multimedia Authoring 3 Homework 2
+
+More information coming soon
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/portfolio', (req, res) => {
+  res.sendFile(__dirname + '/portfolio.html');
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
