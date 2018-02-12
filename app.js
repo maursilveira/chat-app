3c3829de31cd604f72b04a2acacd88dd3448b3d8
Mauricio Silveira <mauricio.machado.silveira@gmail.com>
2018-02-11 23:23:17 -0500

    Add lightbox to fill nickname - Emit nickname on chat - Begin style

--- app.js
+++ app.js
@@ -18,7 +18,14 @@ io.attach(server);
 
 io.on('connection', socket => { // same as function(socket) {...}
   console.log('a user has connected');
-  io.emit('chat message', {for: 'everyone', message: `${socket.id} is here`}); //object
+  // io.emit('connect message', {for: 'everyone', message: `${socket.id} is here`}); //object
+
+  socket.on('connect message', msg => {
+    io.emit('connect message', {for: 'everyone', message: msg});
+  });
+  // socket.on('connect message', () => {
+  //   io.emit('connect message');
+  // });
 
   socket.on('chat message', msg => {
     io.emit('chat message', {for: 'everyone', message: msg});
@@ -26,6 +33,11 @@ io.on('connection', socket => { // same as function(socket) {...}
 
   socket.on('disconnect', () => {
     console.log('a user disconnected');
-    io.emit('disconnect message', `${socket.id} has left`);
+    io.emit('disconnect message', {for: 'everyone', message: `<span>${socket.id}</span> has left the chat`});
+    // io.emit('disconnect', {for: 'everyone', message: socket.nickname});
   });
+
+  // socket.on('disconnect message', msg => {
+  //   io.emit('disconnect message', {for: 'everyone', message: msg});
+  // });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,30 @@
   const socket = io();
 
   let messageList = document.querySelector('ul'),
-      chatForm = document.querySelector('form'),
-      nameInput = document.querySelector('.nickname');
-      chatMessage = document.querySelector('.message');
+      chatForm = document.querySelector('#typing-box'),
+      lightbox = document.querySelector('#lightbox'),
+      nicknameForm = lightbox.querySelector('#nickname-form'),
+      nameInput = document.querySelector('.nickname'),
+      chatMessage = document.querySelector('.message'),
       nickname = null;
 
-  function setNickname() {
-    nickname = this.value;
+  function addNicknameHandler() {
+    nicknameForm.addEventListener('submit', setNickname, false);
+  }
+
+  function setNickname(e) {
+    // nickname = this.value;
+    e.preventDefault();
+    nickname = nicknameForm.querySelector('.nickname').value;
+    lightbox.classList.add('nickname-selected');
+    // msg = `<li class="in-out">${nickname} has entered the chat</li>`;
+    msg = `<span>${nickname}</span> has entered the chat`;
+    socket.emit('connect message', msg);
+    console.log('My nickname is '+nickname);
+    socket.nickname = nickname;
+    console.log(socket);
+    nicknameForm.removeEventListener('submit', setNickname, false);
+    chatForm.addEventListener('submit', handleSendMessage, false);
   }
 
   function appendMessage(msg) {
@@ -19,17 +36,27 @@
     messageList.innerHTML += newMsg;
   }
 
-  function appendDiscMessage(msg) {
-    // debugger;
-    // console.log(msg);
-    let newMsg = `<li>${msg}</li>`;
+  function appendInOutMessage(msg) {
+    console.log(msg);
+    console.log('i am here');
+    let newMsg = `<li class="in-out">${msg.message}</li>`;
     messageList.innerHTML += newMsg;
   }
+
+  // function createDiscMessage() {
+  //   // debugger;
+  //   // console.log(msg);
+  //   // let newMsg = `<li class="in-out"><span>${nickname}</span> has left the room</li>`;
+  //   msg = `<span>${nickname}</span> has left the chat`;
+  //   console.log(msg);
+  //   socket.emit('disconnect message', msg);
+  //   // messageList.innerHTML += newMsg;
+  // }
 
   function handleSendMessage(e) {
     e.preventDefault();
     nickname = (nickname && nickname.length > 0) ? nickname : 'user';
-    msg = `${nickname} says ${chatMessage.value}`;
+    msg = `<span>${nickname}</span> says: ${chatMessage.value}`;
 
     socket.emit('chat message', msg);
     chatMessage.value = '';
@@ -37,8 +64,15 @@
     // debugger;
   }
 
-  nameInput.addEventListener('change', setNickname, false);
-  chatForm.addEventListener('submit', handleSendMessage, false);
+  window.addEventListener('load', addNicknameHandler, false);
+  // chatForm.addEventListener('submit', handleSendMessage, false);
+  // nicknameForm.addEventListener('submit', setNickname, false);
+  // nameInput.addEventListener('change', setNickname, false);
+  // chatForm.addEventListener('submit', handleSendMessage, false);
   socket.addEventListener('chat message', appendMessage, false);
-  socket.addEventListener('disconnect message', appendDiscMessage, false);
+  socket.addEventListener('connect message', appendInOutMessage, false);
+  // socket.addEventListener('disconnect message', appendInOutMessage, false);
+  // socket.addEventListener('disconnect', createDiscMessage, false);
+  socket.addEventListener('disconnect message', appendInOutMessage, false);
+
 })();
