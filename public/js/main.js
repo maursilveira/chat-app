da6df8e8adfbf4aec620a676fcd75eb837920d18
Mauricio Silveira <mauricio.machado.silveira@gmail.com>
2018-02-13 00:00:59 -0500

    Add function to show 'user is typing' message

--- public/js/main.js
+++ public/js/main.js
@@ -7,75 +7,74 @@
       nicknameForm = lightbox.querySelector('#nickname-form'),
       nameInput = document.querySelector('.nickname'),
       chatMessage = document.querySelector('.message'),
+      typingMessage = document.querySelector('.typing'),
       nickname = null;
 
+  // avoid undesired resubmissions of form when reload page
   function addNicknameHandler() {
     nicknameForm.addEventListener('submit', setNickname, false);
   }
 
+  // set nickname and emit connection event
+  // also create event handlers of chat form
   function setNickname(e) {
-    // nickname = this.value;
     e.preventDefault();
     nickname = nicknameForm.querySelector('.nickname').value;
     lightbox.classList.add('nickname-selected');
     msg = `<span>${nickname}</span> has entered the chat`;
     socket.emit('connect message', msg);
-    // socket.nickname = nickname;
     nicknameForm.removeEventListener('submit', setNickname, false);
-    chatForm.addEventListener('change', isTyping, false);
+    chatMessage.addEventListener('keyup', isTyping, false);
     chatForm.addEventListener('submit', handleSendMessage, false);
   }
 
+  // fire on typing and send message with user's nickname
   function isTyping(e) {
+    console.log('from isTyping');
     msg = `${nickname} is typing`;
     socket.emit('typing message', msg);
+    // set timeout to erase 'is typing' message
+    timeout = setTimeout(timeoutTyping, 2000);
   }
 
+  // send empty message to erase 'is typing' message
+  function timeoutTyping() {
+      socket.emit('typing message', '');
+  }
+
+  // write 'is typing' message on screen
   function writeTyping(msg) {
-    let newMsg = `<li class="typing"><p>${msg.message}</p></li>`;
-    messageList.innerHTML += newMsg;
+    typingMessage.innerHTML = msg.message;
   }
 
+  // append chat messages
   function appendMessage(msg) {
     let newMsg = `<li><p>${msg.message}</p></li>`;
     messageList.innerHTML += newMsg;
   }
 
+  // append 'has entered' and 'has left' messages
   function appendInOutMessage(msg) {
-    console.log(msg);
-    console.log('i am here');
     let newMsg = `<li class="in-out"><p>${msg.message}</p></li>`;
     messageList.innerHTML += newMsg;
   }
 
-  // function createDiscMessage() {
-  //   // debugger;
-  //   // console.log(msg);
-  //   // let newMsg = `<li class="in-out"><span>${nickname}</span> has left the room</li>`;
-  //   msg = `<span>${nickname}</span> has left the chat`;
-  //   console.log(msg);
-  //   socket.emit('disconnect message', msg);
-  //   // messageList.innerHTML += newMsg;
-  // }
-
+  // set message to be sent and emit it
   function handleSendMessage(e) {
     e.preventDefault();
-    nickname = (nickname && nickname.length > 0) ? nickname : 'user';
-    msg = `<span>${nickname}</span>: ${chatMessage.value}`;
-    socket.emit('chat message', msg);
-    chatMessage.value = '';
-    return false;
+    // check if message is not empty
+    if(chatMessage.value) {
+      nickname = (nickname && nickname.length > 0) ? nickname : 'user';
+      msg = `<span>${nickname}</span>: ${chatMessage.value}`;
+      socket.emit('chat message', msg);
+      chatMessage.value = '';
+      return false;
+    }
   }
 
   window.addEventListener('load', addNicknameHandler, false);
-  // chatForm.addEventListener('submit', handleSendMessage, false);
-  // nicknameForm.addEventListener('submit', setNickname, false);
-  // nameInput.addEventListener('change', setNickname, false);
-  // chatForm.addEventListener('submit', handleSendMessage, false);
   socket.addEventListener('chat message', appendMessage, false);
   socket.addEventListener('connect message', appendInOutMessage, false);
-  // socket.addEventListener('disconnect message', appendInOutMessage, false);
-  // socket.addEventListener('disconnect', createDiscMessage, false);
   socket.addEventListener('disconnect message', appendInOutMessage, false);
   socket.addEventListener('typing message', writeTyping, false);
 
